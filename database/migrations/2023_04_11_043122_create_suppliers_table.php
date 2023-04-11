91d172b75ff2ddb741516fae5558ba09f1ecebeb
= Ahmad Tito <= ahmadtitohendrawan@gmail.com>
2023-04-11 11:34:38 +0700

    init project

---
+++ database/migrations/2023_04_11_043122_create_suppliers_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('supplier', function (Blueprint $table) {
+            $table->id();
+            $table->string('kode_supplier', 10)->nullable();
+            $table->string('nama_supplier', 50)->nullable();
+            $table->string('telp', 15)->nullable();
+            $table->string('alamat_supplier', 100)->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('suppliers');
+    }
+};
