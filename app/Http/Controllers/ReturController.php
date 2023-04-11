275591fa8378ad22c6a8582b643d59cef1ff8e3f
= Ahmad Tito <= ahmadtitohendrawan@gmail.com>
2023-04-11 11:04:07 +0700

    init project

---
+++ app/Http/Controllers/ReturController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\retur;
+use Illuminate\Http\Request;
+
+class ReturController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\retur  $retur
+     * @return \Illuminate\Http\Response
+     */
+    public function show(retur $retur)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\retur  $retur
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(retur $retur)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\retur  $retur
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, retur $retur)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\retur  $retur
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(retur $retur)
+    {
+        //
+    }
+}
---
+++ database/migrations/2023_04_11_035841_create_returs_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('retur', function (Blueprint $table) {
+            $table->id();
+            $table->date('tanggal')->nullable();
+            $table->string('no_invoice', 10)->nullable();
+            $table->string('kode_barang', 10)->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('returs');
+    }
+};
