074624231f28eacf7d52d411e9d6261cfafcf6cc
= Rizky Angkata <= rizmadoris@gmail.com>
2023-05-09 09:06:27 +0700

    update sidebar

--- resources/views/layout/sidebar.blade.php
+++ resources/views/layout/sidebar.blade.php
@@ -1,4 +1,4 @@
-<aside class="main-sidebar sidebar-dark-primary bg-pink elevation-4">
+<aside class="main-sidebar sidebar-dark-primary bg-primary elevation-4">
     <!-- Brand Logo -->
     <a href="../../index3.html" class="brand-link">
       <img src="{{ asset('assets/dist/img/AdminLTELogo.png') }}" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8') }}">
@@ -6,7 +6,7 @@
     </a>
 
     <!-- Sidebar -->
-    <div class="sidebar bg-pink">
+    <div class="sidebar bg-primary">
         <!-- Sidebar user (optional) -->
         <div class="user-panel mt-3 pb-3 mb-3 d-flex">
           <div class="image">
@@ -41,7 +41,7 @@
               </a>
             </li>
             <li class="nav-item menu-close">
-              <a href="#" class="nav-link active">
+              <a href="#" class="nav-link">
                 <i class="nav-icon far fa-envelope"></i>
                 <p>
                   Transaksi
@@ -51,25 +51,21 @@
               <ul class="nav nav-treeview">
                 <li class="nav-item">
                   <a href="../mailbox/mailbox.html" class="nav-link">
-                    <i class="far fa-circle nav-icon"></i>
                     <p>Barang Masuk</p>
                   </a>
                 </li>
                 <li class="nav-item">
                   <a href="../mailbox/compose.html" class="nav-link">
-                    <i class="far fa-circle nav-icon"></i>
                     <p>Barang Keluar</p>
                   </a>
                 </li>
                 <li class="nav-item">
                   <a href="../mailbox/read-mail.html" class="nav-link">
-                    <i class="far fa-circle nav-icon"></i>
                     <p>Transaksi Barang Masuk</p>
                   </a>
                 </li>
                 <li class="nav-item">
                   <a href="../mailbox/read-mail.html" class="nav-link">
-                    <i class="far fa-circle nav-icon"></i>
                     <p>Transaksi Barang Keluar</p>
                   </a>
                 </li>
