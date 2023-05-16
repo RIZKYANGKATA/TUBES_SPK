225f888ca5895f92c89f676c13b1e2f21f446b0b
= Rizky Angkata <= rizmadoris@gmail.com>
2023-05-16 12:08:31 +0700

    update blade

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -14,7 +14,7 @@
             </div><!-- /.col -->
             <div class="col-sm-6">
               <ol class="breadcrumb float-sm-right">
-                <li class="breadcrumb-item"><a href="{{ url('dasboard') }}">Home</a></li>
+                <li class="breadcrumb-item"><a href="{{ url('dashboard') }}">Home</a></li>
                 <li class="breadcrumb-item active">Dashboard</li>
               </ol>
             </div><!-- /.col -->
