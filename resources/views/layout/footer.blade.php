5971f39409a4d39e8037542da5f9774f37fd7513
= Rizky Angkata <= rizmadoris@gmail.com>
2023-05-23 09:34:00 +0700

    update footer.blade

--- resources/views/layout/footer.blade.php
+++ resources/views/layout/footer.blade.php
@@ -2,5 +2,5 @@
     <div class="float-right d-none d-sm-block">
       <b>PROYEK 1</b>
     </div>
-    <strong>Anggota &copy;  <a href="https://adminlte.io">Ahmad Tito & Rizky Angkata</a>.</strong> All rights reserved.
+    <strong>Sistem Inventory&copy;  <a href="https://adminlte.io">Senggol Dong</a>.</strong> CV. Omega Art
   </footer>
